f083b9bbc3c1dad327bac7d4506ad30749c8cbd8
joshwhieb <joshwhieb@gmail.com>
2023-05-02 18:13:07 -0600

    Remove heap test code

--- src/kernel.c
+++ src/kernel.c
@@ -72,10 +72,5 @@ void kernel_main()
     // initialize interrupt descriptor table
     idt_init();
 
-    void* ptr = kmalloc(50);
-    void* ptr2 = kmalloc(5000);
-    void* ptr3 = kmalloc(5600);
-    kfree(ptr);
-    void* ptr4 = kmalloc(50);
-    if(ptr || ptr2 || ptr3 || ptr4){}
+
 }
